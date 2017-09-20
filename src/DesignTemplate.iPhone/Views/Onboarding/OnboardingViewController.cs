a56121c6010eae71cc7fff059d6c25f6fcce77b1
Max Evtukh <maksevtukh@gmail.com>
2017-09-20 20:11:05 +0300

    Добавил логику переключения карточек и перехода дальше

--- src/DesignTemplate.iPhone/Views/Onboarding/OnboardingViewController.cs
+++ src/DesignTemplate.iPhone/Views/Onboarding/OnboardingViewController.cs
@@ -6,20 +6,53 @@ namespace DesignTemplate.iPhone.Views.Onboarding
 {
     public partial class OnboardingViewController : UIViewController
     {
-        public OnboardingViewController() : base("OnboardingViewController", null)
+        private int _cardIndex = 1;
+        public int CardIndex
+        {
+            get => _cardIndex;
+            set
+            {
+                if (_cardIndex != value)
+                {
+                    _cardIndex = value;
+
+                    if (CardIndex <= _pageControl.Pages)
+                        _pageControl.CurrentPage = CardIndex - 1;
+
+                    if (CardIndex >= _pageControl.Pages)
+                        UIView.Animate(0.3, () => _onboardingButton.SetImage(UIImage.FromBundle("Onboarding_button"), UIControlState.Normal));
+                    else
+                        UIView.Animate(0.3, () => _onboardingButton.SetImage(UIImage.FromBundle("Onboarding_button_next"), UIControlState.Normal));
+                }
+            }
+        }
+
+        public OnboardingViewController() 
+            : base("OnboardingViewController", null)
         {
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            NavigationController.SetNavigationBarHidden(true, false);
-        }
 
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            NavigationController.SetNavigationBarHidden(true, false);
+
+            _onboardingButton.TouchUpInside += (sender, e) =>
+            {
+                if (CardIndex < _pageControl.Pages)
+                    _scrollView.SetContentOffset(new CoreGraphics.CGPoint(UIScreen.MainScreen.Bounds.Width * CardIndex, 0), true);
+
+                if (CardIndex == _pageControl.Pages)
+                {
+                    //TODO: сделать переход на следующий экран
+                }
+            };
+
+            _scrollView.Scrolled += (sender, e) =>
+            {
+                CardIndex = (int)Math.Round(_scrollView.ContentOffset.X / UIScreen.MainScreen.Bounds.Width) + 1;
+            };
         }
     }
 }
--- src/DesignTemplate.iPhone/Views/Onboarding/OnboardingViewController.designer.cs
+++ src/DesignTemplate.iPhone/Views/Onboarding/OnboardingViewController.designer.cs
@@ -1,21 +1,45 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace DesignTemplate.iPhone.Views.Onboarding
 {
-    [Register("OnboardingViewController")]
-    partial class OnboardingViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("OnboardingViewController")]
+	partial class OnboardingViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton _onboardingButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIPageControl _pageControl { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIScrollView _scrollView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (_onboardingButton != null) {
+				_onboardingButton.Dispose ();
+				_onboardingButton = null;
+			}
+
+			if (_pageControl != null) {
+				_pageControl.Dispose ();
+				_pageControl = null;
+			}
+
+			if (_scrollView != null) {
+				_scrollView.Dispose ();
+				_scrollView = null;
+			}
+		}
+	}
 }
